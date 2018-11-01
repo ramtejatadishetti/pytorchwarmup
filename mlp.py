f691bc38483e569229a55ea12784c62bed1f2cf2
rt <ramtejatadishetti@gmail.com>
2018-11-01 15:08:45 -0700

    Custom back prop

--- mlp.py
+++ mlp.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import argparse
+import torch.autograd.function
 
 
 parser = argparse.ArgumentParser(description='MLP example')
@@ -13,6 +14,16 @@ parser.add_argument('--hidden-dim', type=int, default=4, metavar='H',
 parser.add_argument('--output-dim', type=int, default=4, metavar='O',
                         help='ouput dimension size')
 args = parser.parse_args()
+
+class NullyifyGrad(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, input):
+        return input
+    
+    @staticmethod
+    def backward(ctx, grad_output):
+        return torch.zeros_like(grad_output)
 
 class MlpNet(nn.Module):
     def __init__(self, input_dim=4,hidden_dim=4,output_dim=4):
@@ -25,8 +36,11 @@ class MlpNet(nn.Module):
         # Ex: nn.init.<initialization function>(self.fc*.weight) refer https://pytorch.org/docs/master/nn.html#torch-nn-init
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
+        x = self.fc1(x)
+        x = NullyifyGrad.apply(x)
+        x = F.relu(x)
         x = self.fc2(x)
+        x = NullyifyGrad.apply(x)
         return x
 
 
@@ -41,8 +55,8 @@ params = list(net.parameters())
 #print(params)
 
 optimizer = optim.SGD(net.parameters(), lr=0.01)
-for j in range(100): 
-    for i in range(100):
+for j in range(4): 
+    for i in range(1):
         optimizer.zero_grad()
 
         pred = net(random_input[i])
@@ -53,5 +67,5 @@ for j in range(100):
 
         optimizer.step()
 
-        #print(net.fc1.weight, net.fc2.weight)
+        print(net.fc1.weight, net.fc2.weight)
         print(loss)
